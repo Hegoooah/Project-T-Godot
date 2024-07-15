315d45ff14aaab0f67f88af1e39b57d6f5a36d75
sstevec <su19chang@gmail.com>
2024-07-15 16:00:34 -0400

    skill framework

--- Aatrox.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody3D
-
-func _process(delta):
-	animation()
-	
-func animation():
-	$AnimationTree.set("parameters/conditions/idle", velocity == Vector3.ZERO)
-	$AnimationTree.set("parameters/conditions/run", velocity != Vector3.ZERO)
-	
